6cfb1e84c2fa5c356b7dbdb0df95c064adaa846d
Hamish Currie <hamish@currie.com.au>
2016-12-03 00:32:07 +1000

    mostly broken

---
+++ handler.js
@@ -0,0 +1,72 @@
+'use strict';
+const shortid = require('shortid');
+const AWS = require('aws-sdk');  
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+
+
+function getBody(message, event){
+    var body = message + " <hr>"+
+    "<a href=''>Home</a> "+
+    "<a href='list'>List</a> "+
+    "<a href='new'>New</a> "+
+    "<a href='testRedirect'>testRedirect</a> "+
+    "<hr>Lambda called with event values: <pre>" +JSON.stringify(event, null, 2) + "</pre>";
+    return body;
+}
+
+module.exports.smelt = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+  };
+
+  if (event.path === "/smelt/" || event.path === "/smelt" ){
+    response.body = getBody("<h1>Home Page</h1>", event);
+    return callback(null, response);
+  } else if (event.path === "/smelt/new"){
+    var db = new aws.DynamoDB();
+
+    var id = shortid.generate();
+    var model = {
+      ItemId: {"S" : ""},
+    };
+    model.ItemId.S=String(id);
+    db.putItem({
+      TableName: "Items",
+      Item: model
+    }, function (err, data){
+      response.body = getBody("<h1>New Item Saved</h1>" + id, event);
+      return callback(null, response);
+    });
+
+  } else if (event.path === "/smelt/list"){
+    var dbClient = new AWS.DynamoDBClient.DocumentClient();
+    var params = {
+        TableName: "Items"
+    };
+
+    dbClient.scan(params, function(err, data){
+
+      if (err) {
+        response.body = getBody("<h1>Error</h1>", event);
+        callback(null, response);
+      }      
+      var l = "<h1>List</h1> Values: <ul>"
+      for(var i = 0; i < data.Items.length; i++){
+           l = l +"<li>" + data.Items[i].ItemId + "</li>";
+         }
+      l = l + "</ul>";
+      response.body= getBody(l, event);
+      return callback(null, response)
+    });
+  } else {
+    response.body = getBody("<h1>Unexpected path</h1>" + event.path, event);
+    return callback(null, response);
+
+  }
+};
+
+
