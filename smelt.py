6495e8901e906f03aaf6ed66c84516dd0f9795a6
Hamish Currie <hamish@currie.com.au>
2017-01-15 20:47:39 +1000

    initial python version

---
+++ smelt.py
@@ -0,0 +1,60 @@
+import sys
+sys.path.insert(1,'packages')
+
+import boto3
+from flask import Flask, request, redirect, flash, get_flashed_messages, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from jinja2 import Template
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+import logging
+from datetime import datetime
+
+logging.basicConfig(level=logging.INFO)
+
+
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+app = Flask(__name__)
+
+app.debug = True
+
+# set a 'SECRET_KEY' to enable the Flask session cookies
+app.config['SECRET_KEY'] = 'my super secret flask key 7800942rjasdf'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=False
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def frontpage():
+    logging.info("frontpage")
+    logging.info(request)
+    return render_template('home.html', message='Frontpage')
+
+@app.route("/cms/", methods=['GET', 'POST'])
+def cms():
+    logging.info(request)
+    params={'smelt_public_websire_url': "http://smelt-dev-public.s3-website-ap-southeast-2.amazonaws.com/"}
+    return render_template('cms.html', **params)
+
+@app.route('/cms/generate', methods=['GET'])
+def generate():
+    now = datetime.now()
+    body = "This file generated: %s - Hi there" % (str(now))
+
+    s3client = boto3.client('s3')
+    response = s3client.put_object(
+        ACL='public-read',
+        Body=body,
+        Bucket='smelt-dev-public',
+        ContentType='text/html',
+        Key='index.html',
+        StorageClass='STANDARD'
+    )
+    flash("file generated...")
+    return redirect("/cms", code=302)
+
